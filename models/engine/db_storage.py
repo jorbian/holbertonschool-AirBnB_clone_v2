05bfb33630e5226a00ce378f0e8bac240ac1c535
CStamper <cstampok@gmail.com>
2023-03-02 17:00:22 -0600

    update init

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-class TBStorage:
+class DBStorage:
     __engine = None
     __session = None
     def __init__(self):
