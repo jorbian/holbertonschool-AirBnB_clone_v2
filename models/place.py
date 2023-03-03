0fac763766efaa11b43cfc6bbcaa0c8657fba0eb
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-03 11:58:55 -0800

    forign

--- models/place.py
+++ models/place.py
@@ -7,8 +7,8 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
     number_rooms = Column(Integer, default = 0, nullable=False)
