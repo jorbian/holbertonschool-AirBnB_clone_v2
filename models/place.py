9ccca481b6cd413dc0bdcb1e42fd7cdb8891f07b
CStamper <cstampok@gmail.com>
2023-03-03 17:46:35 -0600

    models pycodestyle

--- models/place.py
+++ models/place.py
@@ -17,6 +17,7 @@ place_amenity = Table('place_amenity', Base.metadata,
                              primary_key=True,
                              nullable=False))
 
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
@@ -24,10 +25,10 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default = 0, nullable=False)
-    number_bathrooms = Column(Integer, default = 0, nullable=False)
-    max_guest = Column(Integer, default = 0, nullable=False)
-    price_by_night = Column(Integer, default = 0, nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
