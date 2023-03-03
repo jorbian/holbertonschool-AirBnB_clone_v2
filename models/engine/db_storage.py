c1a12d3d3dae5e6763a6bc9a585a0015c1ffe405
CStamper <cstampok@gmail.com>
2023-03-03 17:43:17 -0600

    engine pycodestyle fixes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,13 +14,14 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 class DBStorage:
     __engine = None
     __session = None
+
     def __init__(self):
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
-                            .format(getenv('HBNB_MYSQL_USER'),
-                                    getenv('HBNB_MYSQL_PWD'),
-                                    getenv('HBNB_MYSQL_HOST'),
-                                    getenv('HBNB_MYSQL_DB')),
-                                    pool_pre_ping=True)
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+                                            getenv('HBNB_MYSQL_USER'),
+                                            getenv('HBNB_MYSQL_PWD'),
+                                            getenv('HBNB_MYSQL_HOST'),
+                                            getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
@@ -50,7 +51,8 @@ class DBStorage:
             if type(cls) == str:
                 cls = eval(cls)
             obj_lst = self.__session.query(cls).all()
-        return {"{}.{}".format(type(obj).__name__, obj.id): obj for obj in obj_lst}
+        return {"{}.{}".format(type(obj).__name__,
+                               obj.id): obj for obj in obj_lst}
 
     def close(self):
         self.__session.close()
