d19ae6c0651d544e6b2cb5f1557c0a8794bb3c2b
CStamper <cstampok@gmail.com>
2023-03-04 17:44:59 -0600

    test checker

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" """
+import models
+import MySQLdb
+import unittest
+from os import getenv
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.engine.base import Engine
+
+
+class TestDBStorage(unittest.TestCase):
+    """ """
+
+
+if __name__ == "__main__":
+    unittest.main()
