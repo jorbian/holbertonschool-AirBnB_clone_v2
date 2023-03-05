a41c4cfe85e09f29350837227f89a7b80dd70879
CStamper <cstampok@gmail.com>
2023-03-04 20:46:31 -0600

    test checker

--- tests/test_console.py
+++ tests/test_console.py
@@ -98,6 +98,17 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertEqual(
                 "** class name missing **\n", f.getvalue())
 
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_reload(self):
+        """Test reload method."""
+        og_session = self.storage._DBStorage__session
+        self.storage.reload()
+        self.assertIsInstance(self.storage._DBStorage__session, Session)
+        self.assertNotEqual(og_session, self.storage._DBStorage__session)
+        self.storage._DBStorage__session.close()
+        self.storage._DBStorage__session = og_session
+
 
 if __name__ == "__main__":
     unittest.main()
