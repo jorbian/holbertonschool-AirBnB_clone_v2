ae4dba6418d5fa087304febfa3d2e5e12a66d1a0
CStamper <cstampok@gmail.com>
2023-03-18 23:34:31 -0500

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,12 +3,7 @@ from os import getenv
 from sqlalchemy import create_engine, inspect, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base, BaseModel
-from models.amenity import Amenity
-from models.place import Place
-from models.city import City
-from models.state import State
-from models.user import User
-from models.review import Review
+from models import city, place, review, state, amenity, user
 
 
 class DBStorage:
