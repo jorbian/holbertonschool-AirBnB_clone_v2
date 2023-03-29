d3e8118efb4e895fb4974a3fe6de049eda2e9c54
jorbian <5533@holbertonstudents.com>
2023-03-28 22:49:22 -0500

    Attempt to fix pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,6 +8,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ DISPLAYS 'Hello HBNB!' """
@@ -19,6 +20,6 @@ def hbnb():
     """ DISPLAYS 'HBNB'. """
     return 'HBNB'
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,6 +8,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ DISPLAYS 'Hello HBNB!' """
@@ -29,4 +30,3 @@ def c(text):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
