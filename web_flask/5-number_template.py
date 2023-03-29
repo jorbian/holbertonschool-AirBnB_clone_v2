671ede2b359472c4c1c1a76862215b703d4d2baf
jorbian <5533@holbertonstudents.com>
2023-03-28 22:58:29 -0500

    Next couple of tasks

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,1 +1,46 @@
 #!/usr/bin/python3
+"""
+STARTS A FLASK WEB APPLICATION
+LISTENING ON 0.0.0.0, PORT 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ DISPLAYS 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ DISPLAYS 'HBNB'. """
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ DISPLAYS 'C' FOLLOWED BY THE VALUE OF <text>. """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """ DISPLAYS 'Python' FOLLOWED BY THE VALUE OF <text> """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ DISPLAYS 'n is a number' ONLY IF <n> IS AN INTEGER """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
